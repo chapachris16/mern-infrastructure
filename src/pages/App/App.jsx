af8ad5b3598d7bfd04d4ae373cf8867da2dedb5e
christopher chapa <chapachristopher16@gmail.com>
2022-06-29 08:18:05 -0500

    big commit

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,30 @@
-
 import './App.css';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistory/OrderHistoryPage';
+import NavBar from '../../components/NavBar';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
+
+
 
 export default function App() {
+  const [user, setUser] = useState(getUser())
+  console.log(user)
   return (
     <main className="App">
-      App
+      {user ? 
+        <>
+          <NavBar user={user} setUser={setUser}/>
+          <Routes>
+            <Route path='/orders/new' element={<NewOrderPage/>} />
+            <Route path='/orders' element={<OrderHistoryPage/>} />
+          </Routes>
+        </>
+        :
+        <AuthPage setUser={setUser}/>
+      }
     </main>
   );
 }
